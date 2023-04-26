fd6c0061433870f7782dc5fc1ce4bca980e88080
Hariprasad Nair <hari27pn@gmail.com>
2023-04-26 19:51:17 +0530

    Update tweetapp.py

--- tweetapp.py
+++ tweetapp.py
@@ -10,7 +10,7 @@ option = st.selectbox('How do you want to perform the data search? You can selec
 word = st.text_input('Please enter a '+option, 'Enter Text Here')
 start = st.date_input("Select the Start Date", datetime.date(2022, 12, 27),key='d1')
 end = st.date_input("Select the End Date", datetime.date(2023, 3, 10),key='d2')
-tweet_c = st.slider('What is the quantity of tweets you desire to gather?', 0, 2000, 100)
+tweet_c = st.slider('What is the quantity of tweets you desire to gather?', 0, 2000, 10)
 tweets_list = []
 
 # Scraping using the TwitterSearchScraper
