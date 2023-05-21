d04d116dbeb73c6517f66b53c9907880a95ec01d
Hariprasad Nair <hari27pn@gmail.com>
2023-05-21 18:24:19 +0530

    Update tweet_app.py

--- tweet_app.py
+++ tweet_app.py
@@ -5,20 +5,22 @@ from datetime import date, timedelta
 import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import numpy as np
-import pymongo 
+import pymongo
+
+###-----------------------------------------     G   U   I   -----------------------------------------###    
 
 #---------------    Front  bar   ---------------#
 
 #1 Heading
 st.title('**:blue[Twitter Scraper]**')
-st.subheader('Enter the Details you want to Scrap')
+st.subheader('Enter your requirements below')
 
 #2 selection :-keyword or Hashtag...........
-selection = st.selectbox('Select your option.',('Keyword', 'Hashtag',))
+selection = st.selectbox('Select your option.',('Keyword.', 'Hashtag.',))
 if selection == 'Keyword.':
-    word = st.text_input('''You selected Keyword, don't use '# 'before the Keyword.''', 'India')
+    word = st.text_input('''You selected Keyword, (ex:-**Elon musk**), Don't use '# 'before the Keyword.''', 'India')
 else:
-    word = st.text_input('''You selected Hashtag, use '# ' before the Keyword.''', '#india')
+    word = st.text_input('''You selected Hashtag, (ex:-**#elonmusk**), use '# '  before the Keyword.''', '#india')
 
 #3 select from date and to date........
 col1, col2, = st.columns(2)
@@ -27,11 +29,14 @@ with col1:
 with col2:
     enddate = st.date_input('Select the End date.',date.today())
 
-#4 Select the count in number min=10 to max=1000....
-count = st.number_input('Number of Tweets,  (Maximum up to **1000**).',min_value=10, max_value=1000,step=10,)
+#4 Select the count in number min=1 to max=1000....
+count = st.number_input('Number of Twitts,  (note:-Minimum **1** to Maximum up to **1000**).',min_value=1, max_value=1000,step=10,)
 
 #5   #Scrap button col1 below :    #show button col2 below :   # download csv col3 below :  #download json col4  below :
 col1, col4,col5,col6, = st.columns(4)
+
+# -------------------------------------------************--------------------------------------------------------#
+
 
 #---------------    Front  bar frame work  (scrap program.) ---------------#
 
@@ -40,7 +45,7 @@ tweets=[]
 
 #Scrap button with program.
 with col1:
-    st.write('''Start **:blue['Scrap']**.''')
+    st.write('''Start Scrap to click below **:blue['Scrap']**.''')
     scrape_button = st.button('**Scrap**')
 
 if "scrape_state" not in st.session_state:
@@ -48,35 +53,36 @@ if "scrape_state" not in st.session_state:
 
 if scrape_button or st.session_state.scrape_state:
     st.session_state.scrape_state = True
+    # st.write('scrape_button running')
 
     if word:
         try:
-            if selection == 'Keyword':
+            if selection == 'Keyword.':
                 for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} lang:en since:{startdate} until:{enddate}').get_items()):
                     if i>count-1:
                         break
                     data=[tweet.date,tweet.user.id,tweet.url,tweet.rawContent,tweet.user.username, tweet.replyCount,
                         tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount,]
                     tweets.append(data)
-                
+                # st.write('keyword running')
             else:
                 for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} lang:en since:{startdate} until:{enddate}').get_items()):
                     if i>count-1:
                         break
                     data=[tweet.date,tweet.user.id,tweet.url,tweet.rawContent,tweet.user.username, tweet.replyCount,
                         tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount,]
-                    tweets.append(data)         
+                    tweets.append(data)
+                # st.write('Hashtag running')
         except Exception as e:
             st.error('Server error (or) Check your internet connection (or) Please Try again after a few minutes', icon='🚨')
     else:
         st.warning(selection,' Must enter atleast one word', icon = "⚠️")
-        
-    # progress bar
+# progress bar
     progress_text = "Operation in progress. Please wait."
-    #     my_bar = st.progress(0, text=progress_text)
-    #     for percent_complete in range(100):
-    #         time.sleep(0.01)
-    #         my_bar.progress(percent_complete + 1, text=progress_text)
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1, text=progress_text)
     st.success('Done . . .')
 
 # Convert the scraped data into Data frame, csv, json, dictionary
@@ -85,10 +91,11 @@ df = pd.DataFrame(tweets, columns=(['Date', 'User ID', 'URL', 'Tweet content', '
 csv = df.to_csv()
 json = df.to_json(orient = 'records')
 data_dict = df.to_dict(orient='records')
+# st.write('Last conversion of data')
 
 # show button
 with col4:
-    st.write('''View Scrapped Data **:blue['Show']**.''')
+    st.write('''You can view in to click below **:blue['Show']**.''')
     show_button = st.button('**Show**')
 
 if "show_state" not in st.session_state:
@@ -97,16 +104,21 @@ if "show_state" not in st.session_state:
 if show_button or st.session_state.show_state:
     st.session_state.show_state = True
     st.dataframe(df)
+    # st.write('Show running')
 
 # Download CSV file button
 with col5:
-    st.write('''Download as CSV file **:blue['CSV']**.''')
+    st.write('''Download CSV file click below **:blue['CSV']**.''')
     st.download_button (label = '**CSV**',data = csv, file_name = f'{word}.csv' ,mime = 'csv/text',)
+    # st.write('CSV running')
 
 # Download JSON file button
 with col6:
-    st.write('''Download as JSON file **:blue['JSON']**.''')
+    st.write('''Download JSON file click below **:blue['JSON']**.''')
     st.download_button (label = '**JSON**',data = json,file_name = f'{word}.json' ,mime = 'json/docx/docxtpl/application',)
+    # st.write('JSON running')
+
+
 
 # #---------------  Side bar ----------------#
 
