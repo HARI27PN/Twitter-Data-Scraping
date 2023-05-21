9e15db0460bd6074549c17c1053646344a7c48de
Hariprasad Nair <hari27pn@gmail.com>
2023-05-21 17:44:45 +0530

    Update tweetapp.py

--- tweetapp.py
+++ tweetapp.py
@@ -1,72 +1,155 @@
-import snscrape.modules.twitter as sntwitter
-import pandas as pd
 import streamlit as st
 import datetime
+import time
+from datetime import date, timedelta
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+import numpy as np
+import pymongo
 
-tweets_df = pd.DataFrame()
-st.write("# TWITTER DATA SCRAPING")
-option = st.selectbox('How do you want to perform the data search? You can select either "Keyword" or "Hashtag" as the search option.',('Keyword', 'Hashtag'))
-word = st.text_input('Please enter a '+option, 'Enter Text Here')
-start = st.date_input("Select the Start Date", datetime.date(2022, 12, 27),key='d1')
-end = st.date_input("Select the End Date", datetime.date(2023, 3, 10),key='d2')
-tweet_c = st.slider('What is the quantity of tweets you desire to gather?', 0, 2000, 10)
-tweets_list = []
+###-----------------------------------------     G   U   I   -----------------------------------------###    
 
-# Scraping using the TwitterSearchScraper
-if word:
-    if option=='Keyword':
-        for i,tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} + since:{start} until:{end}').get_items()):
-            if i>=tweet_c:
-                break
-            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
-        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
-    else:
-        for i,tweet in enumerate(sntwitter.TwitterHashtagScraper(f'{word} + since:{start} until:{end}').get_items()):
-            if i>tweet_c:
-                break            
-            tweets_list.append([ tweet.id, tweet.date,  tweet.content, tweet.lang, tweet.user.username, tweet.replyCount, tweet.retweetCount,tweet.likeCount, tweet.source, tweet.url ])
-        tweets_df = pd.DataFrame(tweets_list, columns=['ID','Date','Content', 'Language', 'Username', 'ReplyCount', 'RetweetCount', 'LikeCount','Source', 'Url'])
+#---------------    Front  bar   ---------------#
+
+#1 Heading
+st.title('**:blue[Twitter Scraper]**')
+st.subheader('Enter your requirements below')
+
+#2 selection :-keyword or Hashtag...........
+selection = st.selectbox('Select your option.',('Keyword.', 'Hashtag.',))
+if selection == 'Keyword.':
+    word = st.text_input('''You selected Keyword, (ex:-**Elon musk**), Don't use '# 'before the Keyword.''', 'India')
 else:
-    st.warning(option,' cannot be empty')
+    word = st.text_input('''You selected Hashtag, (ex:-**#elonmusk**), use '# '  before the Keyword.''', '#india')
 
-# Creating Sidebar 
-with st.sidebar:   
-    st.info('DETAILS ENTERED')
-    if option=='Keyword':
-        st.info('Keyword is '+word)
+#3 select from date and to date........
+col1, col2, = st.columns(2)
+with col1:
+    startdate = st.date_input('Select the Start date.', date.today() - timedelta(days = 100))
+with col2:
+    enddate = st.date_input('Select the End date.',date.today())
+
+#4 Select the count in number min=1 to max=1000....
+count = st.number_input('Number of Twitts,  (note:-Minimum **1** to Maximum up to **1000**).',min_value=1, max_value=1000,step=10,)
+
+#5   #Scrap button col1 below :    #show button col2 below :   # download csv col3 below :  #download json col4  below :
+col1, col4,col5,col6, = st.columns(4)
+
+# -------------------------------------------************--------------------------------------------------------#
+
+
+#---------------    Front  bar frame work  (scrap program.) ---------------#
+
+# scrap_data storage
+tweets=[]
+
+#Scrap button with program.
+with col1:
+    st.write('''Start Scrap to click below **:blue['Scrap']**.''')
+    scrape_button = st.button('**Scrap**')
+
+if "scrape_state" not in st.session_state:
+    st.session_state.scrape_state = False
+
+if scrape_button or st.session_state.scrape_state:
+    st.session_state.scrape_state = True
+    # st.write('scrape_button running')
+
+    if word:
+        try:
+            if selection == 'Keyword.':
+                for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} lang:en since:{startdate} until:{enddate}').get_items()):
+                    if i>count-1:
+                        break
+                    data=[tweet.date,tweet.user.id,tweet.url,tweet.rawContent,tweet.user.username, tweet.replyCount,
+                        tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount,]
+                    tweets.append(data)
+                # st.write('keyword running')
+            else:
+                for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{word} lang:en since:{startdate} until:{enddate}').get_items()):
+                    if i>count-1:
+                        break
+                    data=[tweet.date,tweet.user.id,tweet.url,tweet.rawContent,tweet.user.username, tweet.replyCount,
+                        tweet.retweetCount, tweet.lang, tweet.source, tweet.likeCount,]
+                    tweets.append(data)
+                # st.write('Hashtag running')
+        except Exception as e:
+            st.error('Server error (or) Check your internet connection (or) Please Try again after a few minutes', icon='🚨')
     else:
-        st.info('Hashtag is '+word)
-    st.info('Starting Date is '+str(start))
-    st.info('End Date is '+str(end))
-    st.info("Number of Tweets "+str(tweet_c))
-    st.info("Total Tweets Scraped "+str(len(tweets_df)))
-    x=st.button('Show Tweets',key=1)
+        st.warning(selection,' Must enter atleast one word', icon = "⚠️")
+# progress bar
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+    st.success('Done . . .')
 
-# DOWNLOAD In CSV Format
-@st.cache # Cache is required for conversion to prevent computation on every rerun
-def convert_df(df):    
-    return df.to_csv().encode('utf-8')
+# Convert the scraped data into Data frame, csv, json, dictionary
+df = pd.DataFrame(tweets, columns=(['Date', 'User ID', 'URL', 'Tweet content', 'User Name','Reply count',
+                                    'Retweet count','Language', 'Source', 'Like count',]))
+csv = df.to_csv()
+json = df.to_json(orient = 'records')
+data_dict = df.to_dict(orient='records')
+# st.write('Last conversion of data')
 
-if not tweets_df.empty:
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        csv = convert_df(tweets_df) # CSV
-        c=st.download_button(label="Download data as CSV",data=csv,file_name='Twitter_data.csv',mime='text/csv',)
-    with col2:    # JSON
-        json_string = tweets_df.to_json(orient ='records')
-        j=st.download_button(label="Download data as JSON",file_name="Twitter_data.json",mime="application/json",data=json_string,)
+# show button
+with col4:
+    st.write('''You can view in to click below **:blue['Show']**.''')
+    show_button = st.button('**Show**')
 
-    with col3: # SHOW
-        y=st.button('Show Tweets',key=2)    
-   
-if c:
-    st.success("The Scraped data is Downloaded as .CSV file")  
-if j:
-    st.success("The Scraped data is Downloaded as .JSON file")     
-if x: # DISPLAY
-    st.success("The Scraped data is:")
-    st.write(tweets_df)
-if y: # DISPLAY
-    st.balloons()
-    st.success("Tweets Scraped Successfully:")
-    st.write(tweets_df)
+if "show_state" not in st.session_state:
+    st.session_state.show_state = False
+
+if show_button or st.session_state.show_state:
+    st.session_state.show_state = True
+    st.dataframe(df)
+    # st.write('Show running')
+
+# Download CSV file button
+with col5:
+    st.write('''Download CSV file click below **:blue['CSV']**.''')
+    st.download_button (label = '**CSV**',data = csv, file_name = f'{word}.csv' ,mime = 'csv/text',)
+    # st.write('CSV running')
+
+# Download JSON file button
+with col6:
+    st.write('''Download JSON file click below **:blue['JSON']**.''')
+    st.download_button (label = '**JSON**',data = json,file_name = f'{word}.json' ,mime = 'json/docx/docxtpl/application',)
+    # st.write('JSON running')
+
+
+
+#---------------  Side bar ----------------#
+
+#6 Upload datas into MongoDB data base with data base name ,collection name 
+with st.sidebar:
+    st.title('Storage option')
+    st.write('You can store your data in the **:green[MongoDB]** database.')
+    mongodburl = st.text_input('Enter **:green[MongoDB URL]** link below.', 'mongodb://localhost:27017')
+    st.write('Default Database name: **Twitter**.')
+    collectionname = st.text_input('**Collection name.**',f'{word}')
+    st.write('''Click the **:blue['Upload']** button to start uploading.''')
+    upload_data = st.button('**Upload**')
+
+
+#---------------    Side bar Frame work  (Upload data to Mongodb program.) ---------------#
+
+if "upload_state" not in st.session_state:
+    st.session_state.upload_state = False
+
+if upload_data or st.session_state.show_state:
+    st.session_state.show_state = True
+
+    # connect mongo client
+    client = pymongo.MongoClient(mongodburl)
+    db = client['Twitter']
+    col = db[collectionname]
+    # Insert the datas to Mongodb 
+    col.insert_many(data_dict)
+    # close
+    client.close()
+
+    
+# Stop all program
+st.stop()
